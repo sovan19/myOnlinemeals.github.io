0084d9207659ce6bf7b61858a421d9e148cd0771
sovan19 <66369069+sovan19@users.noreply.github.com>
2020-06-06 23:42:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function submitt(){
+    // var nm=document.getElementById("name").value;
+    // var em=document.getElementById("email").value;
+    // var ph=document.getElementById("phone").value;
+    // var ms=document.getElementById("message").value;
+    // var atpos=em.indexOf("@");
+    // var dotp=em.lastIndexOf(".");
+    // if(nm==""){
+    //     alert("Enter valid name");
+    // }
+    // else if(em==""){
+    //     alert("Enter a valid email-id");
+    // }
+    // else if(ph=="")
+    // {
+    //     alert("Enter a valid phone number");
+        
+    // }
+    // else if("")
+    alert("Thank You for Your Response!! Have a Nice Day :)");
+}
+function resett(){
+    document.getElementsByClassName("form-group").innerHTML="";
+}
